f8d213ff832ea6155280cdd78d8a892993a7dbb6
davidayala001 <davidale699@gmail.com>
2024-07-08 08:02:13 -0400

    David Ayala Moya Davidale6199@gmail.com - Parte final

--- README.md
+++ README.md
@@ -1,56 +1,26 @@
-# Desaf&iacute;o Automatizaci&oacute;n QA
-* Realice el siguiente flujo utilizando Selenium con el lenguaje de programaci&oacute;n que prefiera.
-  
--Ingresar a http://opencart.abstracta.us/index.php?route=common/home
-- Añadir al carro de compras un Ipod Classic
-- Añadir al carro de compras un Imac
-- Proceder a realizar la compra
-- Realizar login con credenciales obtenidas de un archivo externo a elección (basta que sea un email con estructura válida y contraseña)
-- Crear una cuenta
-- Continuar con la compra y llegar a la orden completa
-- Visitar el historial de ordenes y validar resumen de orden 
-- Cerrar sesión
+Proyecto automation
 
-# Validaciones m&iacute;nimas 
-
-- Tomar evidencia de cada producto añadido al carro
-- Validar que los articulos en el carro sean Ipod Classic y Imac
-- Evidencia de creación de la cuenta
-- Evidencia de paso a paso de checkout
-- Validar que despacho y costo sea = Flat Shipping Rate - $5.00
-- Evidencia de costo final de la orden
-- Evidencia de orden completa
-- Evidencia de apartado "Order History" y validar el estado de la compra se encuentre en estado "Pending"
-- Validar datos de dirección de pago v/s los ingresados al crear la cuenta
- 
-
-# Punto extra 
-
-1 Comprar un monitor Apple Cinema 30'' con las siguientes opciones disponibles:
-  - Radio = Large (+30.00)
-  - Checkbox  multiple = 2 (+20.00) y 4 (+40.00)
-  - Texto = Test_1
-  - Select = Yellow (+2.00)
-  - TextArea = Data de prueba
-  - Archivo = Subir archivo .jpg o .png a elección
-  - Fecha = Calendario -> 2022-01-26
-  - Tiempo = Reloj -> 17:25
-  - Fecha y reloj = Calendario y reloj -> 2021-12-24 23:55
-  - Cantidad = 2
-    
-Se ponderará la cantidad de valores ingresados en duro y la documentación del código.
-
-# Entregables
-
--Archivo de entrada de data para la ejecución de la automatización
--Archivo/s de salida (Reporte, log, evidencias tomadas)
--La solución debe contener un README.md con la documentación de la automatización
--Pre requisitos
--Instrucciones para ejecutar
--Detalle Flujo
--Debe ser enviada vía un pull request a este repositorio https://github.com/previred/Desafio_Automatizacion_QA 
--En el detalle del commit debes indicar los siguientes datos (Nombre Completo y Correo Electrónico)
+Estructura (JUnit - Selenium - Java - Maven)
 
 
-# NOTA: 
-El sitio de prueba suele presentar error de l&iacute;mite de accesos, basta repetir la consulta para poder llegar la opci&oacute;n requerida.
+Se generan 3 test:
+
+Flujo compra sin login:
+Genero búsqueda dinámica, se almacena nombres de productos para validar elementos en el carro “Checkout”.
+Almaceno evidencia del paso a paso en Target/Evidencias.
+Se almacena información desplegada en pantalla… número de solicitud en un excel tipo Log Registro de venta y registro nuevo usuario, este es usado para hacer el login y validar la última compra realizada  “order history”.
+
+Flujo login consultar Venta/Compra 3ro producto:
+Consulto último registro en excel generado para realizar búsqueda y validar que coincidan los valores registrados
+
+Flujo compra con login:
+Realizo el flujo login, ingreso los nombres de los productos en el buscador de forma dinámica y los almaceno.
+Realizo flujo de compra, hasta checkout… validando los elementos desplegados contra valores almacenados.
+Almaceno evidencia del paso a paso en Target/Evidencias.
+Se valida información desplegada en pantalla… se almacena número de solicitud en un excel tipo Log Registro de venta.
+Validamos que la compra se la última realizada “order history”
+
+
+Maven:
+Ejecutar “Install mvn”
+Dar Run a “Runner.Class” 
--- src/main/java/TestAutomation/runner.java
+++ src/main/java/TestAutomation/runner.java
@@ -12,7 +12,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -23,56 +25,43 @@ public class runner {
     private WriteExcel writeFile;
     private ChromeDriver driver;
 
-    @Before
-    public void setUp() {
-        if (System.getProperty("os.name").contains("Mac")) {
-            System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
-        }
-            else{
-            System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
-            }
-
-        //verificación de Certificado SSL
-        DesiredCapabilities op = DesiredCapabilities.chrome();
-        op.setAcceptInsecureCerts(true);
-
-        driver = new ChromeDriver(op);
-        driver.manage().window().maximize();
-
-        writeFile = new WriteExcel();
-
-        valorLista =new ArrayList<>();
-    }
 
     @Test
     public void TestAutomation1() throws IOException, InterruptedException {
         nombreCarpetaEvidencias = "Ejecución 1";
         IngresarURL("https://opencart.abstracta.us/index.php?route=common/home");
-        IngresarLogin();
-        AgregarProducto("Ipod Classic");
-        AgregarProducto("Imac");
+        AgregarProducto("iPod Classic");
+        AgregarProducto("iMac");
         IngresarCarro();
-        RealizarCompra();
-
-
-
-
-
+        IngresarDatosCuenta();
+        RealizoValidaciones();
+        RealizoCompra();
         //SaveDataLog();
     }
+
 
     @Test
     public void TestAutomation2() throws IOException, InterruptedException {
         nombreCarpetaEvidencias = "Ejecución 2";
         IngresarURL("https://opencart.abstracta.us/index.php?route=common/home");
-        AgregarProducto("Ipod Classic");
-        AgregarProducto("Imac");
-        IngresarCarro();
-        RegistrarCuenta();
-
-
+        IngresarLogin();
+        IngresarOrderHistory();
+        //ValidarUltimaCompra();
         //SaveDataLog();
     }
+
+
+    @Test
+    public void TestAutomation3() throws IOException, InterruptedException {
+        nombreCarpetaEvidencias = "Ejecución 3";
+        IngresarURL("https://opencart.abstracta.us/index.php?route=common/home");
+        IngresarLogin();
+        AgregarProductoDetalle("Apple Cinema 30");
+        RealizarFlujoCompra();
+        RealizoCompra();
+        //SaveDataLog();
+    }
+
 
     private void IngresarURL(String url) {
         driver.get(url);
@@ -84,18 +73,16 @@ public class runner {
 
 
     private void AgregarProducto(String valor) throws IOException, InterruptedException {
-
         valorLista.add(valor);
         driver.findElementByClassName("form-control").clear();
         driver.findElementByClassName("form-control").sendKeys(valor);
         driver.findElementByClassName("btn-default").click();
         List<WebElement> valor1 = driver.findElementsByClassName("product-thumb");
         screen("producto 0" + contadorProductos);
-        for (WebElement elemento : valor1) {
 
+        for (WebElement elemento : valor1) {
             if (elemento.findElement(By.tagName("h4")).getText().equalsIgnoreCase(valor)) {
                 elemento.findElement(By.className("fa-shopping-cart")).click();
-
                 Thread.sleep(1000);
                 screen("producto agregado 0" + contadorProductos);
                 contadorProductos++;
@@ -104,16 +91,36 @@ public class runner {
         }
     }
 
-    private void IngresarCarro() throws IOException {
-        driver.findElementByClassName("fa-shopping-cart").click();
-        driver.findElementByClassName("clearfix").findElement(By.className("pull-right")).click();
-        screen("Carro de compra");
+    private void AgregarProductoDetalle(String valor) throws IOException, InterruptedException {
+        valorLista.add(valor);
+        driver.findElementByClassName("form-control").clear();
+        driver.findElementByClassName("form-control").sendKeys(valor);
+        driver.findElementByClassName("btn-default").click();
+        List<WebElement> valor1 = driver.findElementsByClassName("product-thumb");
+        screen("producto 0" + contadorProductos);
+        driver.findElementByClassName("product-thumb").click();
+        driver.findElementById("input-option223").findElements(By.tagName("input")).get(0).click();
+        driver.findElementById("input-option223").findElements(By.tagName("input")).get(1).click();
+        driver.findElementById("input-option208").sendKeys("Test_1");
+        driver.findElementById("input-option217").click();
+        Thread.sleep(500);
+        driver.findElementById("input-option217").findElements(By.tagName("option")).get(4).click();
+        driver.findElementById("input-option209").sendKeys("Data de prueba");
+        driver.findElementById("input-option219").sendKeys("2022-01-26");
+        driver.findElementById("input-option221").sendKeys("17:25");
+        driver.findElementById("input-option220").sendKeys("2021-12-24 23:55");
+        screen("Pantalla Ingresar Datos de productos" );
+        driver.findElementById("button-cart").click();
     }
 
+    private void IngresarCarro() throws IOException {
+        driver.findElementByClassName("fa-shopping-cart").click();
+
+        driver.findElementByClassName("clearfix").findElement(By.className("pull-right")).click();
+        screen("Pantalla Carro de compra");
+    }
 
     private void IngresarLogin() throws IOException, InterruptedException {
-
-
         WebElement valor = driver.findElementByClassName("fa-user");
         valor.click();
         valor = driver.findElement(By.className("dropdown-menu-right"));
@@ -121,56 +128,133 @@ public class runner {
         driver.findElementById("input-email").sendKeys(traerValorPropertiesData("usuario"));
         driver.findElementById("input-password").sendKeys(traerValorPropertiesData("clave"));
 
-        screen("Login");
+        screen(" Pantalla Login");
         Thread.sleep(500);
         driver.findElementsByClassName("btn-primary").get(1).click();
-
+        screen("Pantalla Logueado" );
     }
 
-    private void RealizarCompra() throws InterruptedException, IOException {
+    private void RealizarFlujoCompra() throws InterruptedException, IOException {
         Thread.sleep(1500);
         driver.findElementById("button-payment-address").click();
-        Thread.sleep(2500);
+        Thread.sleep(1500);
+        screen("Pantalla Ingresar Datos de productos" );
         driver.findElementById("button-shipping-address").click();
-        Thread.sleep(2500);
+        Thread.sleep(1500);
+        screen("Pantalla Ingresar Datos de productos" );
         driver.findElementById("button-shipping-method").click();
-
+        screen("Pantalla Ingresar Datos de productos" );
         Thread.sleep(2500);
         driver.findElementById("collapse-payment-method").findElements(By.tagName("input")).get(2).click();
         driver.findElementById("button-payment-method").click();
 
         Thread.sleep(1000);
-        screen("Confirmar Order");
-//Validación de productos en checkout
-
+        screen("Pantalla Confirmar Order");
+        //Validación de productos en checkout
+        Assert.assertTrue("", driver.findElementByClassName("").getText().equals(valorLista.get(0)));
         //Validación tipo despacho y costo contra Properties
+        Assert.assertTrue("", driver.findElementByClassName("").getText().equals(traerValorPropertiesData("Despacho")));
+        Assert.assertTrue("", driver.findElementByClassName("").getText().equals(traerValorPropertiesData("Costo")));
     }
 
 
-    private void RegistrarCuenta() throws IOException, InterruptedException {
+    private void IngresarDatosCuenta() throws IOException, InterruptedException {
         Thread.sleep(1500);
         screen("Registrar Cuenta");
+        driver.findElementByClassName("col-sm-6").findElements(By.tagName("input")).get(1).click();
 
         driver.findElementById("button-account").click();
         Thread.sleep(1500);
-        //insertar valores
-        driver.findElementById("button-login").click();
+        screen("Pantalla Ingresar datos cuenta" );
+        //insertar datos personales
+        driver.findElementById("input-payment-firstname").sendKeys(traerValorPropertiesData("nombre"));
+        driver.findElementById("input-payment-lastname").sendKeys(traerValorPropertiesData("apellido"));
+        driver.findElementById("input-payment-email").sendKeys(traerValorPropertiesData("email"));
+        driver.findElementById("input-payment-telephone").sendKeys(traerValorPropertiesData("telefono"));
+        //ingresar dirección
+        driver.findElementById("input-payment-company").sendKeys(traerValorPropertiesData("company"));
+        driver.findElementById("input-payment-address-1").sendKeys(traerValorPropertiesData("direccion1"));
+        driver.findElementById("input-payment-address-2").sendKeys(traerValorPropertiesData("direccion2"));
+        driver.findElementById("input-payment-city").sendKeys(traerValorPropertiesData("ciudad"));
+        driver.findElementById("input-payment-postcode").sendKeys(traerValorPropertiesData("codigoPostal"));
+        screen("Pantalla Datos de cuenta" );
+        driver.findElementById("input-payment-country").click();
+        Thread.sleep(500);
+
+        for (WebElement valor : driver.findElementById("input-payment-country").findElements(By.tagName("option"))) {
+            if (valor.getText().equals(traerValorPropertiesData("pais"))) {
+                valor.click();
+                break;
+            }
+        }
+
         Thread.sleep(1500);
-        driver.findElementById("button-payment-address").click();
+        driver.findElementById("input-payment-zone").click();
+        for (WebElement valor : driver.findElementById("input-payment-zone").findElements(By.tagName("option"))) {
+            if (valor.getText().equals(traerValorPropertiesData("region"))) {
+                valor.click();
+                break;
+            }
+        }
+        screen("Pantalla Datos de cuentas parte 2" );
         Thread.sleep(1500);
-        driver.findElementById("button-shipping-address").click();
+        driver.findElementById("button-guest").click();
         Thread.sleep(1500);
+        screen("Pantalla Datos de envío" );
         driver.findElementById("button-shipping-method").click();
 
         Thread.sleep(1500);
-        driver.findElementById("collapse-payment-method").findElement(By.tagName("input")).click();
+        driver.findElementsByClassName("pull-right").get(5).findElement(By.tagName("input")).click();
+
+        Thread.sleep(500);
         driver.findElementById("button-payment-method").click();
 
         Thread.sleep(1000);
         screen("Confirmar Order");
-        //Validación de productos en checkout
+    }
 
+
+    private void RealizoValidaciones() throws IOException {
+        //Validación de productos en checkout
+        int cantidadTD = driver.findElementsByXPath("//td[1]").size();
+        float cantidad = cantidadTD / 5;
+
+        for (int i = 0; cantidad >= i; ) {
+            for (WebElement valor : driver.findElementsByXPath("//table/tbody/tr[" + i + "]/td[1]")) {
+                if (valor.getText().equals(valorLista.get(0))) {
+                    Assert.assertEquals(valor.getText(), valorLista.get(0));
+                    break;
+                }
+            }
+            i++;
+        }
+        for (int i = 0; cantidad >= i; ) {
+            for (WebElement valor : driver.findElementsByXPath("//table/tbody/tr[" + i + "]/td[1]")) {
+                if (valor.getText().equals(valorLista.get(1))) {
+                    Assert.assertEquals(valor.getText(), valorLista.get(1));
+                    break;
+                }
+            }
+            i++;
+        }
         //Validación tipo despacho y costo contra Properties
+        Assert.assertTrue("", driver.findElementByXPath("//*[@id=\"collapse-checkout-confirm\"]/div/div[1]/table/tfoot/tr[2]/td[1]/strong").getText().equals(traerValorPropertiesData("Despacho")));
+        Assert.assertTrue("", driver.findElementByXPath("//*[@id=\"collapse-checkout-confirm\"]/div/div[1]/table/tfoot/tr[2]/td[2]").getText().equals(traerValorPropertiesData("Costo")));
+    }
+
+    private void RealizoCompra() throws InterruptedException, IOException {
+        driver.findElementById("button-confirm").click();
+        Thread.sleep(2000);
+
+        screen("pantalla Final");
+    }
+
+    private void IngresarOrderHistory() throws InterruptedException, IOException {
+        driver.findElementsByClassName("dropdown-toggle").get(1).click();
+        Thread.sleep(500);
+        driver.findElementsByClassName("dropdown-menu").get(1).click();
+        Thread.sleep(1000);
+        screen("Pantalla Order History");
     }
 
 
@@ -192,9 +276,9 @@ public class runner {
     private void SaveDataLog() throws IOException {
         String filepath = "Excel\\Log.xlsx";
 
-        //writeFile.writeCellValue(filepath, "Hoja1",  0, 0, driver.findElementByClassName("compare_hdn").getText());
+        //writeFile.writeCellValue(filepath, "Hoja1",  0, 0, driver.findElementByClassName();
         int fila = 0;
-        for (WebElement valor : driver.findElementByClassName("inr_tbl").findElements(By.tagName("tr"))) {
+        for (WebElement valor : driver.findElementsByClassName("")) {
             int columna = 0;
             for (WebElement texto : valor.findElements(By.tagName("td"))) {
                 System.out.println(texto.getText());
@@ -205,6 +289,24 @@ public class runner {
         }
     }
 
+    @Before
+    public void setUp() {
+        if (System.getProperty("os.name").contains("Mac")) {
+            System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        }
+
+        //verificación de Certificado SSL
+        DesiredCapabilities op = DesiredCapabilities.chrome();
+        op.setAcceptInsecureCerts(true);
+
+        driver = new ChromeDriver(op);
+        driver.manage().window().maximize();
+
+        writeFile = new WriteExcel();
+        valorLista = new ArrayList<>();
+    }
 
     @After
     public void stopServer() {
